773e0e339eefe0dec17a98b957ce35007ee8423a
JHDeerin <2016deerinjo@gmail.com>
2023-09-14 20:06:15 -0400

    added global styles to svelte version

--- svelteweb/src/routes/+page.svelte
+++ svelteweb/src/routes/+page.svelte
@@ -2,4 +2,27 @@
 	import HomePage from './HomePage.svelte';
 </script>
 
-<HomePage />
+<main id="main" class="parallax-container">
+	<HomePage />
+</main>
+
+<style>
+	:global(.subpage) {
+		display: block;
+		position: relative;
+		overflow: hidden;
+		padding: 1px 0;
+		width: 100vw;
+		min-height: 100%;
+	}
+
+	@media (max-width: 850px) {
+		#about .parallax-background {
+			height: 33%;
+		}
+
+		#resume .parallax-background {
+			height: 25%;
+		}
+	}
+</style>
