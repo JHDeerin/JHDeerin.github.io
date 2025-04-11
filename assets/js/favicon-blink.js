768856f8e764d82cfccc2d058ddda60c93aaedc3
Jake Deerin <2016deerinjo@gmail.com>
2025-04-10 20:33:08 -0400

    minor refactoring

--- assets/js/favicon-blink.js
+++ assets/js/favicon-blink.js
@@ -1,18 +1,18 @@
 function getRelativePathToRoot() {
-    // Get the current path, which will be something like "/path/to/current/page.html"
-    let currentPath = window.location.pathname;
-    // if running on my local filesystem, replace to match the browser format
-    currentPath = currentPath.replace("/C:/Users/2016d/Documents/Projects/github/website-redesign-2025", "");
-    // Split by "/" and remove the filename (last element)
-    const pathParts = currentPath.split('/').filter(Boolean);
-    // Remove the last part if it contains a file extension (likely the HTML file)
-    const dirParts = pathParts.slice(0, pathParts[pathParts.length - 1]?.includes('.') ? -1 : undefined);
-    // Create a relative path with the appropriate number of "../" segments
-    return dirParts.length ? '../'.repeat(dirParts.length) : './';
-  }
+  // Get the current path, which will be something like "/path/to/current/page.html"
+  let currentPath = window.location.pathname;
+  // if running on my local filesystem, replace to match the browser format
+  currentPath = currentPath.replace("/C:/Users/2016d/Documents/Projects/github/website-redesign-2025", "");
+  // Split by "/" and remove the filename (last element)
+  const pathParts = currentPath.split('/').filter(Boolean);
+  // Remove the last part if it contains a file extension (likely the HTML file)
+  const dirParts = pathParts.slice(0, pathParts[pathParts.length - 1]?.includes('.') ? -1 : undefined);
+  // Create a relative path with the appropriate number of "../" segments
+  return dirParts.length ? '../'.repeat(dirParts.length) : './';
+}
 
 function setFavicon(href) {
-    document.getElementById("favicon").href = href;
+  document.getElementById("favicon").href = href;
 }
 
 const openTime = 3000;
@@ -39,25 +39,25 @@ function loadImageAsDataURL(url) {
 }
 
 function swapFavicon(state) {
-    isBlinking = state === "blinking";
-    if (isBlinking) {
-        const blinkTime = Math.round(Math.random()*300 + 200);
-        setFavicon(closedEyesImgData);
-        setTimeout(() => swapFavicon("open"), blinkTime);
-    } else {
-        setFavicon(openEyesImgData);
-        setTimeout(() => swapFavicon("blinking"), openTime);
-    }
+  isBlinking = state === "blinking";
+  if (isBlinking) {
+    const blinkTime = Math.round(Math.random()*300 + 200);
+    setFavicon(closedEyesImgData);
+    setTimeout(() => swapFavicon("open"), blinkTime);
+  } else {
+    setFavicon(openEyesImgData);
+    setTimeout(() => swapFavicon("blinking"), openTime);
+  }
 }
 
 // Load both images as data URLs
 Promise.all([
-    loadImageAsDataURL(`${rootPath}assets/images/favicon-eyes-open.png`),
-    loadImageAsDataURL(`${rootPath}assets/images/favicon-eyes-closed.png`)
-  ]).then(([openData, closedData]) => {
-    openEyesImgData = openData;
-    closedEyesImgData = closedData;
-    // Start the blinking once data is loaded
-    setFavicon(openEyesImgData);
-    setTimeout(() => swapFavicon("open"), openTime);
-  });
+  loadImageAsDataURL(`${rootPath}assets/images/favicon-eyes-open.png`),
+  loadImageAsDataURL(`${rootPath}assets/images/favicon-eyes-closed.png`)
+]).then(([openData, closedData]) => {
+  openEyesImgData = openData;
+  closedEyesImgData = closedData;
+  // Start the blinking once data is loaded
+  setFavicon(openEyesImgData);
+  setTimeout(() => swapFavicon("open"), openTime);
+});
